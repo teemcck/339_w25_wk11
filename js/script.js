383c73e624f38f07940acc6bc7161b9e90bd3457
tristanmac <mccaskilltristan@gmail.com>
2025-03-26 09:48:14 -0400

    Updated JS

--- js/script.js
+++ js/script.js
@@ -4,27 +4,96 @@ let pokemonList = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","C
 
 // Question 1
 
+// Update host name.
+hostName = document.getElementById("host-name");
+hostName.innerHTML = "Tristan";
 
-//Question 2
+// Update collaborator names.
+collab1Name = document.getElementById("collaborator-name");
+hostName.innerHTML = "Qingpeng";
+
+collab2Name = document.getElementById("collaborator-2-name");
+hostName.innerHTML = "test";
+
+// Question 2
+instructionButton = document.getElementById("instruction_button");
+
+instructionButton.addEventListener("click", function() {
+    questions = document.querySelectorAll("question");
+
+    // Turn off display for all questions.
+    for (let question of questions) {
+        question.style.display = "none";
+    }
+});
+
+// Question 3
+firstNameBox = document.getElementById("first_name");
+lastNameBox = document.getElementById("last_name");
+
+// If input is John, autofill Doe.
+firstNameBox.addEventListener("input", function(event) {
+    if (firstNameBox.value === "John") {
+        lastNameBox.value = "Doe";
+    }
+});
+
+// Question 4
+usernameBox = document.getElementById("user_name");
+usernameAlert = document.getElementById("username-alert");
+
+usernameBox.addEventListener("input", function(event) {
+
+    // For each pokemon, check if the username is already taken.
+    for (let pokemon of pokemonList) { 
+        if (usernameBox.value === pokemon) {
+            usernameAlert.innerHTML = "Username already exists";
+            break;
+        } else {
+            usernameAlert.innerHTML = "Unique username created";
+        }
+    }
+});
+
+// Question 5
+passwordBox = document.getElementById("password");
+confirmPasswordBox = document.getElementById("confirm_password");
+passwordAlert = document.getElementById("password-alert");
+hidePasswordButton = document.getElementById("hide-password");
+let showPassword = true;
+
+passwordBox.addEventListener("input", function(event) {
+
+    // Check to see if passwords match.
+    if (passwordBox.value != confirmPasswordBox.value) {
+        passwordAlert.innerHTML = "Passwords does not match";
+    } else {
+        passwordAlert.innerHTML = "Passwords Matches";
+    }
+});
+
+hidePasswordButton.addEventListener("click", function() {
+    if (showPassword) {
+        showPassword = false;
+        passwordBox.type = "password";
+        confirmPasswordBox.type = "password";
+    }
+    else {
+        showPassword = true;
+        passwordBox.type = "password";
+        confirmPasswordBox.type = "password";
+    }
+});
 
 
-//Question 3
+// Question 6
 
 
-//Question 4
+// Question 7
 
 
-//Question 5
+// Question 8
 
 
-//Question 6
-
-
-//Question 7
-
-
-//Question 8
-
-
-//Extra challenge: reset button
+// Extra challenge: reset button
 
